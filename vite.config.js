c6dcbf6e567491f7a1e5bb090a3c93a6844707f6
esguerrasolutions <esguerradesign@gmail.com>
2024-11-06 01:54:35 +0800

    deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/tensixty-app-clone",
   plugins: [react()],
   server: {
     port: 3000,
